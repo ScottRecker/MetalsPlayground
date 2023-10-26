ca47393e2c413cd6ecbc6a13bff52d57349b6c4b
Scott Recker <scott.a.recker@gmail.com>
2023-10-25 23:46:26 -0500

    Initial Commit

---
+++ MetalsPlayground/MetalsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalsPlaygroundApp.swift
+//  MetalsPlayground
+//
+//  Created by Scott Recker on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
