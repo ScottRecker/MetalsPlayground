2893980540c2312a3233138d9e9ee12679fe5b62
Scott Recker <scott.a.recker@gmail.com>
2023-10-26 18:14:22 -0500

    Resize result

--- MetalsPlayground/ContentView.swift
+++ MetalsPlayground/ContentView.swift
@@ -11,9 +11,10 @@ struct ContentView: View {
     var body: some View {
         VStack {
             MetalView()
+                .frame(height: 300)
             Text("Hello Shaders!")
         }
-        .padding()
+//        .padding()
     }
 }
 
